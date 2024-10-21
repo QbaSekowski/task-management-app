40bef459bf0595992aad94aee0012b0f034899a1
Jakub Sekowski <sekowskijakub@gmail.com>
2024-10-21 15:30:03 +0200

    Added project model

---
+++ src/main/java/mate/academy/taskmanagementapp/model/Project.java
@@ -0,0 +1,58 @@
+package mate.academy.taskmanagementapp.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@SQLDelete(sql = "UPDATE projects SET is_deleted = true WHERE id = ?")
+@SQLRestriction("is_deleted = false")
+@Table(name = "projects")
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime startDate;
+    @Column(name = "end_date", nullable = false)
+    private LocalDateTime endDate;
+    @Enumerated(EnumType.STRING)
+    private Status status = Status.INITIATED;
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted = false;
+    @ManyToMany
+    @JoinTable(name = "projects_users",
+            joinColumns = @JoinColumn(name = "project_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private Set<User> users = new HashSet<>();
+
+    public enum Status {
+        INITIATED,
+        IN_PROGRESS,
+        COMPLETED
+    }
+}
