381797a8c46dfa2875edc693272bc915c11d5ac9
Jakub Sekowski <sekowskijakub@gmail.com>
2024-10-23 11:03:28 +0200

    Added CreateProjectRequestDto.

---
+++ src/main/java/mate/academy/taskmanagementapp/dto/project/CreateProjectRequestDto.java
@@ -0,0 +1,17 @@
+package mate.academy.taskmanagementapp.dto.project;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import mate.academy.taskmanagementapp.model.Project;
+
+public record CreateProjectRequestDto(
+        @NotBlank(message = "name may not be blank")
+        String name,
+        String description,
+        @NotNull
+        LocalDateTime startDate,
+        @NotNull
+        LocalDateTime endDate,
+        Project.Status status) {
+}
