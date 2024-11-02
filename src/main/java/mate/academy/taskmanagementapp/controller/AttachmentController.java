d88c344652a2c3e86c11931fedc1efd050b82e31
Jakub Sekowski <sekowskijakub@gmail.com>
2024-11-02 15:14:54 +0100

    Fixed imports in AttachmentController

--- src/main/java/mate/academy/taskmanagementapp/controller/AttachmentController.java
+++ src/main/java/mate/academy/taskmanagementapp/controller/AttachmentController.java
@@ -2,15 +2,17 @@ package mate.academy.taskmanagementapp.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import mate.academy.taskmanagementapp.dto.attachment.AttachmentDto;
-import mate.academy.taskmanagementapp.dto.comment.CreateCommentRequestDto;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
