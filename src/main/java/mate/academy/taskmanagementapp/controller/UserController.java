cd5050564777c23d7b632f8f88268f050a4b648c
Jakub Sekowski <sekowskijakub@gmail.com>
2024-11-18 15:09:09 +0100

    Fixed imports in UserController.

--- src/main/java/mate/academy/taskmanagementapp/controller/UserController.java
+++ src/main/java/mate/academy/taskmanagementapp/controller/UserController.java
@@ -5,7 +5,12 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import mate.academy.taskmanagementapp.dto.user.UserRegistrationRequestDto;
 import mate.academy.taskmanagementapp.dto.user.UserResponseDto;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
