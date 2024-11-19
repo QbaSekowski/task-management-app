73476fb0fb00572bdd43992c3ee8ca493c32b420
Jakub Sekowski <sekowskijakub@gmail.com>
2024-11-19 13:08:00 +0100

    Applied Roles model, repo, modified User model and repo, applied CustomUserDetailsService.

--- src/main/java/mate/academy/taskmanagementapp/repository/user/UserRepository.java
+++ src/main/java/mate/academy/taskmanagementapp/repository/user/UserRepository.java
@@ -1,9 +1,11 @@
 package mate.academy.taskmanagementapp.repository.user;
 
+import java.util.Optional;
 import mate.academy.taskmanagementapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByEmail(String email);
 }
