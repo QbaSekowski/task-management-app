4ce3628f9fa390e25c292d2759fe1f162c4f96d2
Jakub Sekowski <sekowskijakub@gmail.com>
2025-01-12 11:53:13 +0100

    ProjectServiceImpl applied.

--- src/main/java/mate/academy/taskmanagementapp/repository/project/ProjectRepository.java
+++ src/main/java/mate/academy/taskmanagementapp/repository/project/ProjectRepository.java
@@ -1,9 +1,17 @@
 package mate.academy.taskmanagementapp.repository.project;
 
+import java.util.Optional;
+import java.util.Set;
 import mate.academy.taskmanagementapp.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    @Query("FROM Project p JOIN FETCH User u WHERE u.id = :userId AND p.id = :projectId")
+    Optional<Project> findByUserIdAndProjectId(Long userId, Long projectId);
+
+    @Query("FROM Project p JOIN FETCH User u WHERE u.id = :userId")
+    Set<Project> findAllByUserId(Long userId);
 }
