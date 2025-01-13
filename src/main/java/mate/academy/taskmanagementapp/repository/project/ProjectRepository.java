69867a199833b760f7b0ca4663b426be30c8f873
Jakub Sekowski <sekowskijakub@gmail.com>
2025-01-13 14:56:16 +0100

    ProjectRepository fixed.

--- src/main/java/mate/academy/taskmanagementapp/repository/project/ProjectRepository.java
+++ src/main/java/mate/academy/taskmanagementapp/repository/project/ProjectRepository.java
@@ -1,7 +1,7 @@
 package mate.academy.taskmanagementapp.repository.project;
 
+import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import mate.academy.taskmanagementapp.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,9 +9,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
-    @Query("FROM Project p JOIN FETCH User u WHERE u.id = :userId AND p.id = :projectId")
+    @Query("SELECT p FROM Project p JOIN FETCH p.users u WHERE u.id = :userId AND p.id = :projectId")
     Optional<Project> findByUserIdAndProjectId(Long userId, Long projectId);
 
-    @Query("FROM Project p JOIN FETCH User u WHERE u.id = :userId")
-    Set<Project> findAllByUserId(Long userId);
+    @Query("SELECT p FROM Project p JOIN FETCH p.users u WHERE u.id = :userId")
+    List<Project> findAllByUserId(Long userId);
 }
