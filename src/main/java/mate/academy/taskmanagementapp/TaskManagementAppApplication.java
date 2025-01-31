1d7ea46ed41116a428250a40a4d9d1e9268793c1
Jakub Sekowski <sekowskijakub@gmail.com>
2025-01-31 17:18:31 +0100

    Applying docker and .env

--- src/main/java/mate/academy/taskmanagementapp/TaskManagementAppApplication.java
+++ src/main/java/mate/academy/taskmanagementapp/TaskManagementAppApplication.java
@@ -1,12 +1,22 @@
 package mate.academy.taskmanagementapp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TaskManagementAppApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+        System.setProperty("MYSQLDB_USER", dotenv.get("MYSQLDB_USER"));
+        System.setProperty("MYSQLDB_ROOT_PASSWORD", dotenv.get("MYSQLDB_ROOT_PASSWORD"));
+        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+        System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
+        System.setProperty("MYSQLDB_DATABASE", dotenv.get("MYSQLDB_DATABASE"));
+        System.setProperty("DROPBOX_ACCESS_TOKEN", dotenv.get("DROPBOX_ACCESS_TOKEN"));
         SpringApplication.run(TaskManagementAppApplication.class, args);
     }
 }
