99569c2486a9a94a4d775b65cc08c72323d10833
Jakub Sekowski <sekowskijakub@gmail.com>
2025-02-04 10:20:39 +0100

    Small fix in attachment model.

--- src/main/java/mate/academy/taskmanagementapp/model/Attachment.java
+++ src/main/java/mate/academy/taskmanagementapp/model/Attachment.java
@@ -1,14 +1,15 @@
 package mate.academy.taskmanagementapp.model;
 
+import java.time.LocalDateTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -23,7 +24,7 @@ public class Attachment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "task_id", nullable = false)
     private Task task;
     @Column(name = "dropbox_id", nullable = false)
